535d919a21b3b68ce6c027721925adad8ea2dc90
Narasimhan Gopinath <narasimhang@synamedia.com>
2022-01-30 18:53:34 +0530

    refactored dir structure

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # hackerrank-practice
+I shall be adding all the solutions that I have submitted in platforms such as hackerrank, leetcode etc.
+I have also added solutions for programming questions i have faced in interviews.
+This repo also contains the javascript learning material along with samples.
+I shall also add NodeJs and ReactJs samples in future here.
---
+++ leetcode/ZigZagTraversal.js
@@ -0,0 +1,45 @@
+function zigZagTraversal(root) {
+    const results = [];
+    const lot = (node, level) => {
+      if (!node) return;
+  
+      if (!results[level]) {
+        results[level] = [];
+      }
+      results[level].push(node.value);
+      level++;
+  
+      lot(node.left, level);
+      lot(node.right, level);
+    }
+    lot(root, 0);
+    results.map((bucket, i) => {
+      if (i % 2 > 0) {
+        bucket.reverse();
+      }
+    });
+    console.log(results);
+  }
+
+  class Node {
+    constructor(value) {
+      this.value = value;
+      this.right = null;
+      this.left = null;
+    }
+  }
+  
+  function main() {
+    var root = new Node(1);
+    root.left = new Node(2);
+    root.right = new Node(3);
+    root.left.left = new Node(4);
+    root.left.right = new Node(5);
+    root.right.left = new Node(6);
+    root.right.right = new Node(7);
+    root.right.left.left = new Node(8);
+    root.right.left.right = new Node(9);
+    zigZagTraversal(root);
+  }
+  main();
+  
