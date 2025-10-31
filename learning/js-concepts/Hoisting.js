327d409aab37ce9c0324e499a6c7e832ed0b8b4b
Narasimhan Gopinath <narasimhan.g@hotmail.com>
2025-10-31 12:35:24 +0530

    What is hoisting in JS
    Add comments explaining variable hoisting in JavaScript.

---
+++ learning/js-concepts/Hoisting.js
@@ -0,0 +1,7 @@
+/*
+var is hoisted in global.
+let and const are hoisted in temperol dead zone.
+
+Accessing var before declaration will result in undefined.
+Accessing let and const before declaration will give an error - variable is not defined.
+*/
